f7711ca4c7ef2e294789cdc395a7d1138798251b
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2016-10-21 11:02:17 +0700

    first commit

---
+++ Assets/Scripts/ServerManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Rendering;
+
+public class ServerManager : MonoBehaviour {
+
+	public NetworkManager manager;
+	void Awake()
+	{
+		// If running in headless mode, start the server immediately
+		if(SystemInfo.graphicsDeviceType == GraphicsDeviceType.Null)
+		{
+			manager = GetComponent<NetworkManager>();
+			manager.StartServer();
+		}
+	}
+}
